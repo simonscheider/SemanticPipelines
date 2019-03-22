053c36f34a61138c1b345ede43a8eec6b0c55b3a
Scheider <s.scheider@uu.nl>
2019-03-22 21:20:35 +0100

    tool taxonomy added

--- cleanWfTaxonomy.py
+++ cleanWfTaxonomy.py
@@ -20,6 +20,9 @@ from rdflib import Namespace
 from urlparse import urlparse
 import os
 
+TOOLS=rdflib.Namespace("http://geographicknowledge.de/vocab/GISTools.rdf#")
+ADA = rdflib.Namespace("http://geographicknowledge.de/vocab/AnalysisData.rdf#")
+
 """Helper stuff"""
 def load_rdf( g, rdffile, format='turtle' ):
     #print("load_ontologies")
@@ -45,16 +48,55 @@ def n_triples( g, n=None ):
         print( '  Triples: +'+str(len(g)-n) )
     return len(g)
 
-def main():
-    input = 'CoreConceptData.ttl'
+def cleanOWLOntology(input = 'CoreConceptData.ttl'):
+    print 'Clean OWL ontology!'
     ccdontology = load_rdf(rdflib.Graph(),input)
+    print 'Running inferences:'
     ccdontology = run_inferences(ccdontology)
     taxonomy = rdflib.Graph()
-    taxonomy += ccdontology.triples( (None, RDFS.subClassOf, None) )
+    print 'Extracting subClassOf triples:'
+    taxonomy += ccdontology.triples( (None, RDFS.subClassOf, None) ) #Keeping only subClassOf statements and classes
     taxonomy += ccdontology.triples( (None, RDF.type, OWL.Class) )
     n_triples(taxonomy)
-    output = 'CoreConceptData_tax.rdf'
-    taxonomy.serialize(destination=output,format = "application/rdf+xml")
+    print 'Cleaning blank node triples:'
+    taxonomyclean = rdflib.Graph()
+    for (s,p,o) in taxonomy: #Removing triples that stem from blanknodes
+        if type(s) != BNode and type(o) != BNode:
+            taxonomyclean.add((s,p,o))
+    n_triples(taxonomyclean)
+    #add common upper class for all data types, including spatial attributes and spatial data sets
+    taxonomyclean.add((ADA.ValueList,RDFS.subClassOf,TOOLS.DType))
+    taxonomyclean.add((ADA.SpatialDataSet,RDFS.subClassOf,TOOLS.DType))
+    return taxonomyclean
+
+def extractToolOntology(tooldesc='ToolDescription.ttl'):
+    print 'Extract Tool ontology!'
+    output = rdflib.Graph()
+    tools = load_rdf(rdflib.Graph(),tooldesc)
+    for (s,p,o) in tools.triples((None, TOOLS.implements, None)):
+        output.add((o,RDFS.subClassOf,s))
+        output.add((s, RDF.type, OWL.Class))
+        output.add((o, RDF.type, OWL.Class))
+        #add common upper class for all tool types
+        output.add((s,RDFS.subClassOf,TOOLS.Tool))
+    n_triples(output)
+    return output
+
+
+def main():
+    dt = 'CoreConceptData_tax.ttl'
+    tax = cleanOWLOntology()
+    tooltax =extractToolOntology()
+    tax.serialize(destination=dt,format = "turtle")
+    to = 'ToolDescription_tax.ttl'
+    tooltax.serialize(destination=to,format = "turtle")
+    final = rdflib.Graph()
+    final.parse(dt, format='turtle')
+    final.parse(to, format='turtle')
+    final.serialize(destination='GISTaxonomy.rdf', format = "application/rdf+xml")
+
+
+
 
 
 
